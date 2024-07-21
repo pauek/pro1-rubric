3b0dca4f3b1c62adac280ea90231d9d0a679741e
Pau Fernández <pau.fernandez@upc.edu>
2024-07-21 20:49:39 +0200

    Polish

--- components/Quadre.tsx
+++ components/Quadre.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Quadre({ children }: { children: React.ReactNode }) {
   return (
-    <div className="border border-yellow-200 shadow-md rounded-lg bg-yellow-50 px-5 py-0">
+    <div className="border border-yellow-200 shadow-md rounded-lg bg-yellow-50 px-6 py-4">
       {children}
     </div>
   );
