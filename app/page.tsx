423983ceaf144971460da0afa3746e837af18168
Pau Fernández <pau.fernandez@upc.edu>
2024-07-21 23:09:09 +0200

    Polish

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Rubric from "@/components/Rubric.mdx";
 
 export default function Home() {
   return (
-    <main className="p-8 max-w-[50rem]">
+    <main className="p-8 max-w-[50rem] m-auto min-h-[150em]">
       <Rubric />
     </main>
   );
